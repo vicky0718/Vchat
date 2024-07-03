bbdb76eab153ab45477b809c668631a4cdf920a0
vicky0718 <vicky187kvs@gmail.com>
2024-07-03 10:57:29 +0530

    Commit index

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require('./config/connectDB')
-const router = require('./routes/index')
-const cookiesParser = require('cookie-parser')
-const {app, server} = require("./socket/index")
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
+const cookieParser = require('cookie-parser');
+const { app, server } = require("./socket/index");
 
-//const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: []
-  })
-);
-app.use(express.json())
-app.use(cookiesParser())
+// Ensure FRONTEND_URL is set in your .env file
+// e.g., FRONTEND_URL=http://192.168.1.x:4000
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Define allowed methods
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 8080;
 
@@ -25,11 +26,19 @@ app.get("/", (request, response) => {
   });
 });
 
-//api endpoint
-app.use('/api', router)
+// API endpoint
+app.use('/api', router);
 
-connectDB().then(()=>{
+// Error handling middleware (optional but recommended)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+connectDB().then(() => {
   server.listen(PORT, () => {
     console.log("server is running at ==> " + PORT);
   });
-})
+}).catch(err => {
+  console.error('Failed to connect to database:', err);
+});
